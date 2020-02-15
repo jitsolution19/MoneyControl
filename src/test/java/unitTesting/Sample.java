cfd00708b81de1c7e403c9626f1ce23d0770f569
jit <jit@jit-Ideapad-Z570>
2020-02-15 22:41:25 +0530

    update the maven

--- src/test/java/unitTesting/Sample.java
+++ src/test/java/unitTesting/Sample.java
@@ -9,4 +9,16 @@ public void test1()
 	System.out.println("Test1");
 }
 	
+@Test()
+public void test2()
+{
+	System.out.println("Test2");
+}
+
+@Test()
+public void test3()
+{
+	System.out.println("Test3");
+}
+
 }
