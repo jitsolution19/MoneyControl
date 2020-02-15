a1ab7997c6e72c5972cb6290acce19086d7acff7
jit <jit@jit-Ideapad-Z570>
2020-02-15 22:43:45 +0530

    update

--- src/test/java/unitTesting/Sample.java
+++ src/test/java/unitTesting/Sample.java
@@ -3,22 +3,19 @@ package unitTesting;
 import org.junit.Test;
 
 public class Sample {
-@Test()
-public void test1()
-{
-	System.out.println("Test1");
-}
-	
-@Test()
-public void test2()
-{
-	System.out.println("Test2");
-}
+	@Test()
+	public void test1() {
+		System.out.println("Test1");
+	}
 
-@Test()
-public void test3()
-{
-	System.out.println("Test3");
-}
+	@Test()
+	public void test2() {
+		System.out.println("Test2");
+	}
+
+	@Test()
+	public void test3() {
+		System.out.println("Test3");
+	}
 
 }
