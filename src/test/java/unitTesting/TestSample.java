4064e437ec5882bede1b72b227ae4450a682908c
jit <jit@jit-Ideapad-Z570>
2020-02-22 16:55:40 +0530

    added the test cases

---
+++ src/test/java/unitTesting/TestSample.java
@@ -0,0 +1,21 @@
+package unitTesting;
+
+import org.junit.Test;
+
+public class TestSample {
+	@Test()
+	public void test1() {
+		System.out.println("Test1");
+	}
+
+	@Test()
+	public void test2() {
+		System.out.println("Test2");
+	}
+
+	@Test()
+	public void test3() {
+		System.out.println("Test3");
+	}
+
+}
