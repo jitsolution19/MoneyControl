dba1bc3f2fef1fc59fafb25ca11d1eeef9a3c947
jit <jit@jit-Ideapad-Z570>
2020-03-01 23:35:07 +0530

    changed logic

--- src/test/java/scenario/BrowserLaunchTest.java
+++ src/test/java/scenario/BrowserLaunchTest.java
@@ -2,52 +2,110 @@ package scenario;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserLaunchTest {
 
 //	@Test
-	public void launch_chrome()
-	{
+	public void launch_chrome() {
 		System.out.println("Launch Chrome Browser");
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions option = new ChromeOptions();
 		option.addArguments("--start-maximized");
-		WebDriver driver= new ChromeDriver(option);		
-		driver.get("https://github.com/bonigarcia/webdrivermanager/blob/master/README.md");
-	}
-	
-	@Test
-	public void launch_firefox()
-	{
-		System.out.println("Launch Firefox Browser");
-		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
-//		driver.get("https://timesofindia.indiatimes.com/");
-		driver.get("https://economictimes.indiatimes.com/");
-		
+		WebDriver driver = new ChromeDriver(option);
+//		driver.get("https://github.com/bonigarcia/webdrivermanager/blob/master/README.md");
+//	driver.get("https://economictimes.indiatimes.com/");
+		driver.get("https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/");
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 		List<WebElement> newslink = driver.findElements(By.tagName("a"));
 		Iterator<WebElement> counter = newslink.iterator();
-		while(counter.hasNext())
-		{
+		while (counter.hasNext()) {
 			WebElement items = counter.next();
-			if(items.isDisplayed()) {
+			if (items.isDisplayed()) {
 				System.out.println(items.getText());
 			}
+
 		}
+		driver.quit();
 	}
-	
-	
-	
-	
+
+//	@Test
+//	public void launch_firefox()
+//	{
+//		System.out.println("Launch Firefox Browser");
+//		WebDriverManager.firefoxdriver().setup();
+////		FirefoxOptions option = new FirefoxOptions();
+////		option.setProfile(new FirefoxProfile());
+////		option.addPreference("dom.webnotifications.enabled", false);
+////		DesiredCapabilities  cap= new DesiredCapabilities();
+////		cap.setCapability("-marionette", false);
+////		cap.setCapability("unhandledPromptBehavior", "dismiss");
+//		WebDriver driver = new FirefoxDriver();
+//		
+//		driver.get("https://timesofindia.indiatimes.com/");
+////		driver.get("https://economictimes.indiatimes.com/");
+//		
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//		
+//		List<WebElement> newslink = driver.findElements(By.tagName("a"));
+//		Iterator<WebElement> counter = newslink.iterator();
+//		while(counter.hasNext())
+//		{
+//			WebElement items = counter.next();
+//			if(items.isDisplayed()) {
+//				System.out.println(items.getText());
+//			}
+//		driver.quit();
+//		}
+//		
+//	}
+
+	@Test
+	public void launch_firfox() {
+		System.out.println("Launch firefox Browser");
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+//		driver.get("https://github.com/bonigarcia/webdrivermanager/blob/master/README.md");
+		driver.get("https://economictimes.indiatimes.com/");
+//		driver.get("https://www.moneycontrol.com/");
+
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		int attempt = 0;
+		while (attempt < 3) {
+			try {
+				List<WebElement> newslink = driver.findElements(By.tagName("a"));
+				Iterator<WebElement> counter = newslink.iterator();
+				while (counter.hasNext()) {
+					WebElement items = counter.next();
+					if (items.isDisplayed()) {
+						System.out.println(items.getText());
+					}
+
+				}
+			} catch (StaleElementReferenceException e) {
+
+			}
+			attempt++;
+		}
+
+		driver.quit();
+	}
+
 }
