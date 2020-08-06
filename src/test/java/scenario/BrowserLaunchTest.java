82c8b2a7cf7d878cfde27b5d5c95b4fa9cdb4926
jit <jit@jit-Ideapad-Z570>
2020-08-06 08:31:16 +0530

    changes pushed

--- src/test/java/scenario/BrowserLaunchTest.java
+++ src/test/java/scenario/BrowserLaunchTest.java
@@ -13,23 +13,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserLaunchTest {
 
-//	@Test
+	@Test
 	public void launch_chrome() {
 		System.out.println("Launch Chrome Browser");
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions option = new ChromeOptions();
 		option.addArguments("--start-maximized");
-		WebDriver driver = new ChromeDriver(option);
-//		driver.get("https://github.com/bonigarcia/webdrivermanager/blob/master/README.md");
-//	driver.get("https://economictimes.indiatimes.com/");
+		WebDriver driver = new ChromeDriver(option);	
 		driver.get("https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/");
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -38,44 +33,13 @@ public class BrowserLaunchTest {
 		Iterator<WebElement> counter = newslink.iterator();
 		while (counter.hasNext()) {
 			WebElement items = counter.next();
-			if (items.isDisplayed()) {
+			if (items.isDisplayed()&&items.isEnabled()) {
 				System.out.println(items.getText());
 			}
 
 		}
 		driver.quit();
 	}
-
-//	@Test
-//	public void launch_firefox()
-//	{
-//		System.out.println("Launch Firefox Browser");
-//		WebDriverManager.firefoxdriver().setup();
-////		FirefoxOptions option = new FirefoxOptions();
-////		option.setProfile(new FirefoxProfile());
-////		option.addPreference("dom.webnotifications.enabled", false);
-////		DesiredCapabilities  cap= new DesiredCapabilities();
-////		cap.setCapability("-marionette", false);
-////		cap.setCapability("unhandledPromptBehavior", "dismiss");
-//		WebDriver driver = new FirefoxDriver();
-//		
-//		driver.get("https://timesofindia.indiatimes.com/");
-////		driver.get("https://economictimes.indiatimes.com/");
-//		
-//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//		
-//		List<WebElement> newslink = driver.findElements(By.tagName("a"));
-//		Iterator<WebElement> counter = newslink.iterator();
-//		while(counter.hasNext())
-//		{
-//			WebElement items = counter.next();
-//			if(items.isDisplayed()) {
-//				System.out.println(items.getText());
-//			}
-//		driver.quit();
-//		}
-//		
-//	}
 
 	@Test
 	public void launch_firfox() {
@@ -96,7 +60,7 @@ public class BrowserLaunchTest {
 				Iterator<WebElement> counter = newslink.iterator();
 				while (counter.hasNext()) {
 					WebElement items = counter.next();
-					if (items.isDisplayed()) {
+					if (items.isDisplayed()&&items.isEnabled()) {
 						System.out.println(items.getText());
 					}
 
